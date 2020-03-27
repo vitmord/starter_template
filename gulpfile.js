88bd084527391622c7ace1a5e8f2fbb6fbe78bd5
vitmord <vitmord@gmail.com>
2020-03-27 13:49:43 +0300

    #02

--- gulpfile.js
+++ gulpfile.js
@@ -13,102 +13,117 @@ const fileinclude = require("gulp-file-include");
 const del = require("del");
 const browserSync = require("browser-sync").create();
 
+const posthtml = require("gulp-posthtml");
+const attrsSorter = require('posthtml-attrs-sorter');
+
 function style() {
-  return gulp
-    .src("./src/scss/**/*.scss")
-    .pipe(
-      plumber({
-        errorHandler: notify.onError(function(err) {
-          return {
-            title: "Styles",
-            sound: true,
-            message: err.message
-          };
-        })
-      })
-    )
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(
-      autoprefixer(["last 15 versions", "> 1%", "ie 8", "ie 7"], {
-        cascade: true
-      })
-    )
-    .pipe(
-      cleanCSS({
-        compatibility: "ie8"
-      })
-    )
-    .pipe(
-      rename({
-        suffix: ".min"
-      })
-    )
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("./build/css/"))
-    .pipe(browserSync.stream());
+    return gulp
+        .src("./src/scss/**/*.scss")
+        .pipe(
+            plumber({
+                errorHandler: notify.onError(function (err) {
+                    return {
+                        title: "Styles",
+                        sound: true,
+                        message: err.message
+                    };
+                })
+            })
+        )
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(
+            autoprefixer(["last 15 versions", "> 1%", "ie 8", "ie 7"], {
+                cascade: true
+            })
+        )
+        .pipe(
+            cleanCSS({
+                compatibility: "ie8"
+            })
+        )
+        .pipe(
+            rename({
+                suffix: ".min"
+            })
+        )
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("./build/css/"))
+        .pipe(browserSync.stream());
 }
 
 function js() {
-  return gulp
-    .src("./src/js/**/*.js")
-    .pipe(concat("script.js"))
-    .pipe(
-      uglify({
-        // toplevel: true
-      })
-    )
-    .pipe(
-      rename({
-        suffix: ".min"
-      })
-    )
-    .pipe(gulp.dest("./build/js"));
+    return gulp
+        .src("./src/js/**/*.js")
+        .pipe(concat("script.js"))
+        .pipe(
+            uglify({
+                // toplevel: true
+            })
+        )
+        .pipe(
+            rename({
+                suffix: ".min"
+            })
+        )
+        .pipe(gulp.dest("./build/js"));
 }
 
 function html() {
-  return gulp
-    .src("./src/*.html")
-    .pipe(
-      plumber({
-        errorHandler: notify.onError(function(err) {
-          return {
-            title: "Html include",
-            sound: true,
-            message: err.message
-          };
-        })
-      })
-    )
-    .pipe(
-      fileinclude({
-        prefix: "@@"
-      })
-    )
-    .pipe(gulp.dest("./build"));
+    return gulp
+        .src("./src/*.html")
+        .pipe(
+            plumber({
+                errorHandler: notify.onError(function (err) {
+                    return {
+                        title: "Html include",
+                        sound: true,
+                        message: err.message
+                    };
+                })
+            })
+        )
+        .pipe(posthtml([
+            attrsSorter()
+        ]))
+        .pipe(
+            fileinclude({
+                prefix: "@@"
+            })
+        )
+        .pipe(gulp.dest("./build"));
 }
 
 function watch() {
-  browserSync.init({
-    server: {
-      baseDir: "./build"
-    }
-  });
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
 
-  gulp.watch("./src/scss/**/*.scss", style);
-  gulp.watch("./src/js/**/*.js", js).on("change", browserSync.reload);
-  gulp.watch("./src/**/*.html", html).on("change", browserSync.reload);
+    gulp.watch("./src/scss/**/*.scss", style);
+    gulp.watch("./src/js/**/*.js", js).on("change", browserSync.reload);
+    gulp.watch("./src/**/*.html", html).on("change", browserSync.reload);
 }
 
 function clean() {
-  return del("build");
+    return del("build");
 }
 
-const build = gulp.series(clean, style, js, html);
+function htmlAttrsSorter() {
+    return gulp.src('./source/*.html')
+        .pipe(posthtml([
+            attrsSorter()
+        ]))
+        .pipe(gulp.dest('./source'));
+};
+
+const build = gulp.series(clean, style, js, htmlAttrsSorter, html);
 
 exports.style = style;
 exports.js = js;
 exports.html = html;
+exports.htmlAttrsSorter = htmlAttrsSorter;
 exports.watch = watch;
 exports.clean = clean;
 
